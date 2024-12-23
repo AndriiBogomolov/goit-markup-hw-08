d7c86039f5c8bd40f4c5fe6618bb92b389828e8c
Andrii Bogomolov <bogomolov.andrii@gmail.com>
2024-12-23 19:48:55 +0100

    fix/styles_part_3

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,36 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
+    const isModalHidden = refs.modal.classList.contains('is-hidden');
+
+    if (isModalHidden) {
+      // during opening modal window
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+      document.body.classList.add('modal-open');
+      refs.modal.classList.remove('is-hidden');
+    } else {
+      // during closing modal window
+      refs.modal.classList.add('is-hidden');
+
+      // waiting for the animation completion before resetting styles
+      refs.modal.addEventListener(
+        'transitionend',
+        () => {
+          document.body.style.paddingRight = '';
+          document.body.classList.remove('modal-open');
+        },
+        { once: true },
+      );
+    }
   }
 })();
